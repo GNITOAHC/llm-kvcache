4bfb54bb1ab76e374238c1cb9a2c016a9fa6ac7a
ryan98153 <110703007@nccu.edu.tw>
2025-02-13 15:46:00 +0800

    feat: rouge1 and rougeL score similarity

--- cag/similarity.py
+++ cag/similarity.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer, util
+from rouge_score import rouge_scorer
 
 # Use a lightweight sentence-transformer
 bert_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
@@ -17,3 +18,13 @@ def bert(response, ground_truth):
     cosine_score = util.pytorch_cos_sim(query_embedding, text_embedding)
 
     return cosine_score.item()
+
+def rouge1(response, ground_truth):
+    scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=True)
+    scores = scorer.score(response, ground_truth)
+    return scores['rouge1'].fmeasure
+
+def rougeL(response, ground_truth):
+    scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+    scores = scorer.score(response, ground_truth)
+    return scores['rougeL'].fmeasure
