a6cc7564e11cea312ffcffbf99facffe0c5a75f9
GINTOAHC <chaotingchen10@gmail.com>
2025-02-18 20:24:40 +0800

    refactor: Update time calculation script

--- results_summary/timescale.py
+++ results_summary/timescale.py
@@ -1,41 +1,41 @@
 r"""
-\multirow{6}{*}{Small}
-        & BM25 top-3   & 0.0009 & 0.6628 & -      & -      \\
-        & BM25 top-10  & 0.0012 & 1.5139 & -      & -      \\
-        & Dense top-3  & 0.4516 & 0.9280 & -      & -      \\
-        & Dense top-10 & 0.4946 & 0.9280 & -      & -      \\
-        & CAG          & -      & 0.7705 & 0.0005 & -      \\
-        & w/o CAG      & -      & 8.6273 & -      & 0.0692 \\
+    \multirow{6}{*}{Small}
+        & Sparse RAG, Top-3   & 0.0008 & 0.7406 \\
+        & Sparse RAG, Top-10  & 0.0012 & 1.5595 \\
+        & Dense RAG, Top-3  & 0.4849 & 1.0093 \\
+        & Dense RAG, Top-10 & 0.3803 & 2.6608 \\
+        & CAG          & -      & 0.8512 \\
+        & In-Context Learning      & -      & 9.3197 \\
     \midrule
     \multirow{6}{*}{Medium}
-        & BM25 top-3   & 0.0008 & 0.6528 & -      & -      \\
-        & BM25 top-10  & 0.0012 & 1.5202 & -      & -      \\
-        & Dense top-3  & 0.4927 & 0.9138 & -      & -      \\
-        & Dense top-10 & 0.4824 & 2.6128 & -      & -      \\
-        & CAG          & -      & 1.2688 & 0.0005 & -      \\
-        & w/o CAG      & -      & 25.5114 & -      & 0.3589 \\
-    \midrule
+        & Sparse RAG, Top-3   & 0.0008 & 0.7148  \\
+        & Sparse RAG, Top-10  & 0.0012 & 1.5306  \\
+        & Dense RAG, Top-3  & 0.4140 & 0.9566 \\
+        & Dense RAG, Top-10 & 0.4171 & 2.6361 \\
+        & CAG          & -      & 1.4078 \\
+        & In-Context Learning      & -      & 26.3717 \\
+    \midrule 
     \multirow{6}{*}{Large}
-        & BM25 top-3   & 0.0008 & 0.6667 & -      & -      \\
-        & BM25 top-10  & 0.0012 & 1.5175 & -      & -      \\
-        & Dense top-3  & 0.4123 & 0.9331 & -      & -      \\
-        & Dense top-10 & 0.4100 & 2.6447 & -      & -      \\
-        & CAG          & -      & 2.2631 & 0.0005 & -      \\
-        & w/o CAG      & -      & 92.0824 & -      & 86.6877 \\
+        & Sparse RAG, Top-3   & 0.0008 & 0.6667 \\
+        & Sparse RAG, Top-10  & 0.0012 & 1.5175 \\
+        & Dense RAG, Top-3  & 0.4123 & 0.9331 \\
+        & Dense RAG, Top-10 & 0.4100 & 2.6447 \\
+        & CAG          & -      & 2.2631 \\
+        & In-Context Learning      & -      & 92.0824 \\
     \bottomrule
 """
 
-sm_bm25_top3 = [0.0009, 0.6628]
-sm_bm25_top10 = [0.0012, 1.5139]
-sm_dense_top3 = [0.4516, 0.9280]
-sm_dense_top10 = [0.4946, 0.9280]
-sm_cag = [0.7705, 0.0005]
+sm_bm25_top3 = [0.0008, 0.7148]
+sm_bm25_top10 = [0.0012, 1.5595]
+sm_dense_top3 = [0.4849, 1.0093]
+sm_dense_top10 = [0.3803, 2.6608]
+sm_cag = [0.8512, 0.0000]
 
-md_bm25_top3 = [0.0008, 0.6528]
-md_bm25_top10 = [0.0012, 1.5202]
-md_dense_top3 = [0.4927, 0.9138]
-md_dense_top10 = [0.4824, 2.6128]
-md_cag = [1.2688, 0.0005]
+md_bm25_top3 = [0.0008, 0.7148]
+md_bm25_top10 = [0.0012, 1.5306]
+md_dense_top3 = [0.4140, 0.9566]
+md_dense_top10 = [0.4171, 2.6361]
+md_cag = [1.4078, 0.0000]
 
 lg_bm25_top3 = [0.0008, 0.6667]
 lg_bm25_top10 = [0.0012, 1.5175]
@@ -54,6 +54,8 @@ threshold = 0.01
 
 def main():
     largest_sum = max([sum(x) for x in sm + md + lg])
+
+    print(f"Largest sum: {largest_sum}")
     normalized = lambda x: x / largest_sum
 
     for _, dataset in enumerate(data):
